787bb0bb804b79bea15683cd1fc2f3fc2383146c
KazuyaMiyashita <kazuya.miyashita@septeni-original.co.jp>
2019-12-18 20:26:22 +0900

    なんかシンプルにできた

--- src/main/scala/mycats/registrationExample/FormValidator.scala
+++ src/main/scala/mycats/registrationExample/FormValidator.scala
@@ -12,35 +12,35 @@ trait FormValidator {
       age: Int
   ): Either[List[DomainValidation], RegistrationData]
 
-  def validateUserNameEither(userName: String): Either[DomainValidation, String] =
+  def validateUserName(userName: String): Either[DomainValidation, String] =
     Either.cond(
       userName.matches("^[a-zA-Z0-9]+$"),
       userName,
       UsernameHasSpecialCharacters
     )
 
-  def validatePasswordEither(password: String): Either[DomainValidation, String] =
+  def validatePassword(password: String): Either[DomainValidation, String] =
     Either.cond(
       password.matches("(?=^.{10,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$"),
       password,
       PasswordDoesNotMeetCriteria
     )
 
-  def validateFirstNameEither(firstName: String): Either[DomainValidation, String] =
+  def validateFirstName(firstName: String): Either[DomainValidation, String] =
     Either.cond(
       firstName.matches("^[a-zA-Z]+$"),
       firstName,
       FirstNameHasSpecialCharacters
     )
 
-  def validateLastNameEither(lastName: String): Either[DomainValidation, String] =
+  def validateLastName(lastName: String): Either[DomainValidation, String] =
     Either.cond(
       lastName.matches("^[a-zA-Z]+$"),
       lastName,
       LastNameHasSpecialCharacters
     )
 
-  def validateAgeEither(age: Int): Either[DomainValidation, Int] =
+  def validateAge(age: Int): Either[DomainValidation, Int] =
     Either.cond(
       age >= 18 && age <= 75,
       age,
--- src/main/scala/mycats/registrationExample/FormValidatorCats.scala
+++ src/main/scala/mycats/registrationExample/FormValidatorCats.scala
@@ -6,32 +6,6 @@ import cats.implicits._
 
 object FormValidatorCats extends FormValidator {
 
-  private type ValidationResult[A] = Validated[NonEmptyChain[DomainValidation], A]
-
-  private def toValidated[A](e: Either[DomainValidation, A]): ValidationResult[A] = {
-    Validated.fromEither(e).leftMap(NonEmptyChain(_))
-  }
-
-  private def validateUserName(userName: String): ValidationResult[String] = {
-    toValidated(validateUserNameEither(userName))
-  }
-
-  private def validatePassword(password: String): ValidationResult[String] = {
-    toValidated(validatePasswordEither(password))
-  }
-
-  private def validateFirstName(firstName: String): ValidationResult[String] = {
-    toValidated(validateFirstNameEither(firstName))
-  }
-
-  private def validateLastName(lastName: String): ValidationResult[String] = {
-    toValidated(validateLastNameEither(lastName))
-  }
-
-  private def validateAge(age: Int): ValidationResult[Int] = {
-    toValidated(validateAgeEither(age))
-  }
-
   override def validateForm(
       username: String,
       password: String,
@@ -39,12 +13,13 @@ object FormValidatorCats extends FormValidator {
       lastName: String,
       age: Int
   ): Either[List[DomainValidation], RegistrationData] = {
-    val v: ValidationResult[RegistrationData] = (
-      validateUserName(username),
-      validatePassword(password),
-      validateFirstName(firstName),
-      validateLastName(lastName),
-      validateAge(age)
+
+    val v: ValidatedNec[DomainValidation, RegistrationData] = (
+      validateUserName(username).toValidatedNec,
+      validatePassword(password).toValidatedNec,
+      validateFirstName(firstName).toValidatedNec,
+      validateLastName(lastName).toValidatedNec,
+      validateAge(age).toValidatedNec
     ).mapN(RegistrationData)
 
     v.toEither.leftMap(_.toList)
